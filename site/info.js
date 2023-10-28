7693643228de9f718589f8ff395d759a016ab2b4
Franco Jalil <jalilfranco9@gmail.com>
2023-10-27 22:15:18 -0300

    get started -> v

--- site/info.js
+++ site/info.js
@@ -4,8 +4,16 @@ function getStartedFlux() {
     window.location.href = "/login?next=pricing";
 }
 
+function getStartedFluxV() {
+   
+    // llevarlo a /signup con algun contexto
+    window.location.href = "/login?next=verification";
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let getStartedButton = document.getElementById('getStarted');
+    let getStartedVerification = document.getElementById('getStartedVerification');
 
     getStartedButton.addEventListener('click', getStartedFlux);
+    getStartedVerification.addEventListener('click', getStartedFluxV);
 });
